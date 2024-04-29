b4bdd3e9f24a86294e27f588fc6e9d1e73bd196b
Aiden Foo <aidenfoo154@gmail.com>
2024-04-29 11:49:08 -0700

    installing flowbite and adding dropdown to navbar

--- src/Components/NavbarComponent/page.tsx
+++ src/Components/NavbarComponent/page.tsx
@@ -6,7 +6,25 @@ export default function NavbarComponent() {
   return (
     <div className='bg-gradient-to-r from-[#AEE6D9] to-[#3EBE9F] min-h-28 flex justify-between'>
     <img src={Logo.src} alt="Logo" className='ml-10 h-1/2' />
-    <div className='h-16 w-16 rounded-full bg-blue-500 mr-5 mt-5 border-solid border-2 border-black '></div>
+    <div className='h-16 w-16 rounded-full bg-blue-500 mr-5 mt-5 border-solid border-2 border-black' data-dropdown-toggle="dropdown "></div>
+
+<div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+      </li>
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+      </li>
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+      </li>
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+      </li>
+    </ul>
+</div>
+
     </div>
   )
 }
--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
 import NavbarComponent from "../../Components/NavbarComponent/page";
+import Paintbrush from '@/Assets/Paintbrush.png'
+import Pencil from '@/Assets/pencil.png'
 
 export default function Dashboard() {
   return (
@@ -9,7 +11,14 @@ export default function Dashboard() {
       <div className="grid grid-cols-2">
         <div className="min-h-screen flex justify-center mt-10 cols-3">
           <div className="flex flex-col items-center hammersmith">
-            <div className="h-96 w-96 rounded-full bg-blue-500 border-solid border-2 border-black"></div>
+            <div className="h-96 w-96 rounded-full bg-blue-500 border-solid border-2 relative border-black">
+              <div className="absolute bottom-0 left-0">
+                <img src={Paintbrush.src} alt="change image"/>
+              </div>
+              <div className="absolute bottom-0 right-0">
+                <img src={Pencil.src} alt="random color"/>
+              </div>
+            </div>
             <div className="text-center mt-10 text-4xl font-bold">Username</div>
             <div className="text-center text-4xl font-bold mt-5">Joined now</div>
           </div>
