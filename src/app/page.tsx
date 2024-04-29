c0a8744f5b51ab755f01b6535bf282727618617f
Aiden Foo <aidenfoo154@gmail.com>
2024-04-29 11:21:55 -0700

    dashboard initial set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
 import NavbarComponent from "@/Components/NavbarComponent/page";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+
 
 export default function Home() {
+  const router = useRouter();
+    const Handle = () => {
+        router.push("/Dashboard")
+    };  
   return (
-    
+    <>
     <NavbarComponent/>
+      <button onClick={Handle} className="bg-black text-white">Go to Dashboard</button>
+    </>
+    
+    
   );
 }
