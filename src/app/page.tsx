fedf86ebfaa401598c020bad3f2d4dd3af3c9d19
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-04-29 11:25:27 -0700

    create page edits pt1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,22 @@ export default function Home() {
       <div className="grid place-items-center mt-32">
         <div className="w-[730px] h-[764px] bg-[#3EBE9F]">
           <div className="flex justify-center">
+            <div className="w-[80%]">
+              <div className="grid place-items-center">
+                <p>Create Account</p>
 
+                <p>Username</p>
+                <input type="text" />
+                <p>Password</p>
+                <input type="text" />
+                <p>Confirm Password</p>
+                <input type="text" />
+
+                <button>Create</button>
+                <p>Already have an account? <span><a href="">Login</a></span> </p>
+
+              </div>
+            </div>
           </div>
         </div>
       </div>
