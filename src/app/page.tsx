c601ff9116079c950583efab210b52f46e44c2e9
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-04-29 11:39:22 -0700

    create page edits pt2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,33 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  
+
   const router = useRouter();
-    const Handle = () => {
-      router.push("/Dashboard")
-  }; 
-  
+  const Handle = () => {
+    router.push("/Dashboard")
+  };
+
   return (
-    <div className="bg-[#F1FFFC] min-h-full h-screen">
-    <NavbarComponent/>
-    <button onClick={Handle} className="bg-black text-white">Go to Dashboard</button>
-    
+    <div className="bg-[#F1FFFC] min-h-screen h-full">
+      <NavbarComponent />
+      <button onClick={Handle} className="bg-black text-white">Go to Dashboard</button>
+
       <div className="grid place-items-center mt-32">
-        <div className="w-[730px] h-[764px] bg-[#3EBE9F]">
+        <div className="w-[730px] h-[764px] mb-20 bg-[#3EBE9F]">
           <div className="flex justify-center">
             <div className="w-[80%]">
               <div className="grid place-items-center">
-                <p>Create Account</p>
+                <p className="holtwood">Create Account</p>
 
-                <p>Username</p>
-                <input type="text" />
-                <p>Password</p>
-                <input type="text" />
-                <p>Confirm Password</p>
-                <input type="text" />
+                <p className="hammersmith">Username</p>
+                <input className="hammersmith" type="text" />
+                <p className="hammersmith">Password</p>
+                <input className="hammersmith" type="text" />
+                <p className="hammersmith">Confirm Password</p>
+                <input className="hammersmith" type="text" />
 
-                <button>Create</button>
-                <p>Already have an account? <span><a href="">Login</a></span> </p>
+                <button className="hammersmith">Create</button>
+                <p className="hammersmith">Already have an account? <span><a href="">Login</a></span> </p>
 
               </div>
             </div>
