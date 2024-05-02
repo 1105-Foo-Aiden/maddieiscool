861c27a694508e0a199e6d458eee6236e974d85a
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-05-02 11:30:07 -0700

    changed some names

--- src/Components/sortableComponent.tsx
+++ src/Components/sortableComponent.tsx
@@ -1,23 +1,16 @@
-import React, { FC, useState } from "react";
+import { ItemType } from "@/Interfaces/Interfaces";
+import React, { useState } from "react";
 import { ReactSortable } from "react-sortablejs";
 
-interface ItemType {
-    id: number;
-    title: string;
-    desc: string;
-    priority: string;
-    userPFP: string;
-}
-
 export const sortableComponent = () => {
-    const [state, setState] = useState<ItemType[]>([
+    const [dummyData, setDumyData] = useState<ItemType[]>([
         { id: 1, title: "Test Login", desc: "A short description of the task you are viewing", priority: "2", userPFP: "string" },
         { id: 2, title: "Test Create Account", desc: "A short description of the task you are viewing", priority: "5", userPFP: "string" },
     ]);
 
     return (
-        <ReactSortable list={state} setList={setState}>
-            {state.map((item) => (
+        <ReactSortable list={dummyData} setList={setDumyData}>
+            {dummyData.map((item) => (
                 <div className="inline-flex w-[23%]" key={item.id}>
                     <div className="bg-white rounded-2xl px-6 h-[168px]">
                         <p className="hammersmith text-2xl">{item.title}</p>
--- src/Interfaces/Interfaces.ts
+++ src/Interfaces/Interfaces.ts
@@ -6,3 +6,11 @@ export interface IUserData {
     username: string,
     password: string
 }
+
+export interface ItemType {
+    id: number;
+    title: string;
+    desc: string;
+    priority: string;
+    userPFP: string;
+}
